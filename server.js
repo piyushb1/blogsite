dbd12291895db6e90ac03652fd579396788a6d01
Piyush Bankar <piyushbankar18@gmail.com>
2020-07-23 19:26:18 +0530

    deploy

--- server.js
+++ server.js
@@ -15,6 +15,7 @@ mongoose.connect('mongodb://localhost/blog', {
 app.set('view engine', 'ejs');
 
 app.use(express.urlencoded({ extended: false }));
+
 app.use(methodOverride('_method'));
 
 app.get('/', async (req, res) => {
